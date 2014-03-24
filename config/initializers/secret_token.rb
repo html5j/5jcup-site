772ef1bb387fe510f44471d8142e1b1ed5975d58
Hal Seki <hal@plants-web.jp>
2014-03-24 22:42:39 +0900

    exec bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5jcup::Application.config.secret_token = 'afa6420d8e69f9cb50635e778abfe60da812f7ec23293ed820ad36f3d2b645b07420d5d168f615b5d5c2439f87cfb0719f373878be2b6afbfb536e61be8c960d'
