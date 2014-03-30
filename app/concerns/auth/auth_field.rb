df8c039b231b0f263322b4f1ac2ba5e340d11d4b
Hal Seki <hal@plants-web.jp>
2014-03-31 00:16:06 +0900

    try-fix

--- app/concerns/auth/auth_field.rb
+++ app/concerns/auth/auth_field.rb
@@ -2,6 +2,7 @@ module Auth
   module AuthField
     extend ActiveSupport::Concern
     included do
+      include ::Mongoid::Document
       field :require_login,         type: Boolean, localize: true
     end
   end
