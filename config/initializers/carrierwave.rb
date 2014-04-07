3104df817321461f901042d064db7b04fa751a2d
Hal Seki <hal@plants-web.jp>
2014-04-07 14:13:20 +0900

    add staging

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,17 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.root = File.join(Rails.root, 'public')
 
+  when :staging
+    # the following configuration works for Amazon S3
+    config.storage          = :fog
+    config.fog_credentials  = {
+      provider:                 'AWS',
+      aws_access_key_id:        ENV['S3_KEY_ID'],
+      aws_secret_access_key:    ENV['S3_SECRET_KEY'],
+      region:                   ENV['S3_BUCKET_REGION']
+    }
+    config.fog_directory    = ENV['S3_BUCKET']
+
   when :production
     # the following configuration works for Amazon S3
     config.storage          = :fog
