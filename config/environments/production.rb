f88c7b03eef6f2d4d2b3c980ac9e51752e0a46f2
Hal Seki <hal@plants-web.jp>
2014-04-09 01:16:29 +0900

    change smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,12 @@ Html5jcup::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => 25,
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
+    :domain         => ENV['SENDGRID_DOMAIN'],
+    :enable_starttls_auto => true
   }
 
 
