4eefa3ed12578308e6c490485ea2a5aa254ef9be
Hal Seki <hal@plants-web.jp>
2014-04-09 01:52:18 +0900

    change from mail address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = 'please-change-me@config-initializers-devise.com'
+  config.mailer_sender = 'info@fivejcup.heroku.com'
 end
