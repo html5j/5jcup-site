174b583737b556a87d75b7b62a15e56516ff3758
Hal Seki <hal@plants-web.jp>
2014-04-09 01:48:26 +0900

    add devise.rb

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+Devise.setup do |config|
+  # ==> Mailer Configuration
+  # Configure the e-mail address which will be shown in DeviseMailer.
+  config.mailer_sender = 'please-change-me@config-initializers-devise.com'
+end
