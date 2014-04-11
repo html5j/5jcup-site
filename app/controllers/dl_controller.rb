e81110478717b677c14a80bf3c62889b08f84a02
Hal Seki <hal@plants-web.jp>
2014-04-12 06:50:42 +0900

    add dl_controller

--- app/controllers/dl_controller.rb
+++ app/controllers/dl_controller.rb
@@ -1,2 +1,16 @@
 class DlController < ApplicationController
+  include Locomotive::Routing::SiteDispatcher
+  include Locomotive::Render
+  include Locomotive::ActionController::LocaleHelpers
+  before_filter :require_site
+  def show
+    slug =  params['slug']
+    type =  params['type']
+    @page ||= self.locomotive_page('/materials/' + slug)
+    #logger.debug(@page.render(self.locomotive_context({})))
+    context = self.locomotive_context({})
+    url = context['material'][type]
+    render :text => url
+    #redirect_to url
+  end
 end
