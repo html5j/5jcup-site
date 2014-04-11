b76c7f7a808599e45c96674602c99a5d84a4c74e
Hal Seki <hal@plants-web.jp>
2014-04-11 12:31:09 +0900

    add works test

---
+++ spec/controllers/works_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe WorksController do
+  login_admin
+  it "should have a current_user" do
+    # note the fact that I removed the "validate_session" parameter if this was a scaffold-generated controller
+    subject.current_user.should_not be_nil
+  end
+
+end
