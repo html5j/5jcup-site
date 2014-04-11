05852874a747c206e54e847d562bebe7fa65dd19
Hal Seki <hal@plants-web.jp>
2014-04-11 12:24:41 +0900

    remove unused test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,10 +7,4 @@ describe UsersController do
     subject.current_user.should_not be_nil
   end
 
-  it "should get index" do
-    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
-    # the valid_session overrides the devise login. Remove the valid_session from your specs
-    get 'index'
-    response.should be_success
-  end
 end
