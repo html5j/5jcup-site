83b84601aeab53a6fd01b599eb946684557d9693
Hal Seki <hal@plants-web.jp>
2014-04-14 07:09:29 +0900

    change address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,6 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = 'info@fivejcup.heroku.com'
+  config.mailer_sender = 'noreply@html5j.org'
   config.scoped_views = true
 end
