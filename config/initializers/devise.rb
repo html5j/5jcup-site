03594a20555e40f952d50fc3c03c73171ef95349
Hal Seki <hal@plants-web.jp>
2014-04-14 00:59:08 +0900

    localized confirmation mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,4 +2,5 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
   config.mailer_sender = 'info@fivejcup.heroku.com'
+  config.scoped_views = true
 end
