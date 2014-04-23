ef237125bf606fee3b50c79b644e80b1cbc22a46
Hal Seki <hal@plants-web.jp>
2014-04-23 21:51:29 +0900

    data into session

--- app/controllers/locomotive/public/pages_controller.rb
+++ app/controllers/locomotive/public/pages_controller.rb
@@ -25,6 +25,7 @@ module Locomotive
 
       def show
         if (current_user)
+          session['username'] = current_user.name
           render_locomotive_page(nil, {'username' => current_user.name})
         else
           render_locomotive_page
