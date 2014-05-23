edfdc4cd06a724fe0bfcc33ad5822dc166711759
Hal Seki <hal@plants-web.jp>
2014-05-23 16:52:16 +0900

    show error message when mailaddress is not correct.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -16,5 +16,32 @@ class Users::PasswordsController < Devise::PasswordsController
       }
     end
   end
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      @page ||= self.locomotive_page('/sentresetlink')
+      respond_to do |format|
+        format.html {
+           render :inline => @page.render(self.locomotive_context({ 'user' => self.resource}))
+        }
+      end
+    else
+      logger.debug(resource.errors)
+      flash[:error] = ""
+      resource.errors.full_messages.each do |msg|
+        flash[:error] << '<li>' + msg + '</li>'
+      end
+      @page ||= self.locomotive_page('/passwordreset')
+      respond_to do |format|
+        format.html {
+           render :inline => @page.render(self.locomotive_context({ 'user' => self.resource, 'error' => flash[:error]}))
+        }
+      end
+    end
+  end
+
 
 end
