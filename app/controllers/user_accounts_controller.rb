a374cb2a2bf1877bb17d17e462cfdae0f1611456
Hal Seki <hal@plants-web.jp>
2014-06-11 16:25:24 +0900

    remove UserAccount of specified provider

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -2,7 +2,8 @@ class UserAccountsController < ApplicationController
   before_filter :authenticate_user!
 
   def deauth
-    render :json => []
+    current_user.user_accounts.where(provider: params[:provider]).first.delete
+    render :json => [return:'success']
   end
 
 end
